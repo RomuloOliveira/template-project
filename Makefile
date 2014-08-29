b087b70bd55118b1a9e1c3162a9596da7e160abc
Rômulo Oliveira <romulo.ac.oliveira@gmail.com>
2014-08-29 19:11:07 -0300

    smarter makefile and default environment variables

--- Makefile
+++ Makefile
@@ -1,12 +1,14 @@
-all: clean build tests
+all: clean build
 
 clean:
 	# Remove compiled, temp files, etc.
 
 build:
+	./bin/configure.sh
 	# Install dependencies, compile, link and build
 
-tests:
+tests: export ENV=test
+tests: build
 	# Start tests
 
 .PHONY: tests build
---
+++ bin/configure.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Configuration script for your project
+
+# Setup the environment
+# Uses ENV variable to set custom environment. Default is `devel`
+DEFAULT_ENV=devel
+
+if [ ${ENV} ]; then
+    source "env/$ENV"
+else
+    source "env/$DEFAULT_ENV"
+fi
+
+# If you have a `local` environment, use it
+if [ -f env/local ]; then
+    source env/local
+fi
+
+echo "$ENV"
