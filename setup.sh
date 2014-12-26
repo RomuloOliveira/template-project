de8a57ffc539d3af5703f5b91fd694ec83d239a3
Rômulo Oliveira <romulo.ac.oliveira@gmail.com>
2014-12-26 21:33:32 -0200

    Fix setup problems
    This really closes #1

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 # Script for setup basic folder structure
+wget https://github.com/RomuloOliveira/template-project/raw/master/template-project.tar.gz
 tar --skip-old-files --extract --verbose --file template-project.tar.gz
+rm template-project.tar.gz
